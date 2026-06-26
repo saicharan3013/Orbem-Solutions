f782945c32fcc50b3741a5f36fd1eed3c684c99a
Sai Charan <saicharan30.g@gmail.com>
2026-06-27 00:58:50 +0530

    Fix API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: 'https://orbem-solutions-backend.onrender.com/api'
+});
 
-api.interceptors.request.use(config => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem('orbem_token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
 api.interceptors.response.use(
-  res => res,
-  err => {
+  (res) => res,
+  (err) => {
     if (err.response?.status === 401) {
       localStorage.removeItem('orbem_token');
       localStorage.removeItem('orbem_user');
